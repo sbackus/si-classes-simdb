4143f583cbbee77450d1f09b99344ba46253a545
Bob Breznak <bob.breznak@gmail.com>
2013-03-14 23:48:51 -0400

    Generate vanilla Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SIMDB::Application.config.secret_token = '69332aef5e774a2b704d8c9f587b0067a5c5d504ed6382bfbda4873f2d9ceabe6324a1d293cb43636cada3eb0f916d7d21901ad913a1b12b6c686722f1a2f9cd'
