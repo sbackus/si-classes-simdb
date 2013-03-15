a2c806bd80c354320fb385ea4f1c73ef5ef7134f
Bob Breznak <bob.breznak@gmail.com>
2013-03-15 10:56:36 -0400

    Add model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :description, :released_on, :poster_url, :tagline, :tmdb_id, :imdb_id
 
   paginates_per 10
+
+  validates :title, :description, :released_on, presence: true
 end
